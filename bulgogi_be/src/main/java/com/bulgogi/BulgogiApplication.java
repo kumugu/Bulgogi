69adaec8b6a8ca6f07903599ff1af7ec9f811221
kumugu <rmarn018@gmail.com>
2025-03-07 12:16:21 +0900

    create be_project
    - springboot 3.4
     - web, mysql driver, security

---
+++ bulgogi_be/src/main/java/com/bulgogi/BulgogiApplication.java
@@ -0,0 +1,13 @@
+package com.bulgogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulgogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulgogiApplication.class, args);
+	}
+
+}
